425f663311dd130131fa7484e2961d9d96c02bd4
jianghaiqiang <jianghaiqiang@360.cn>
2023-04-18 15:59:56 +0800

    add traffic limit

--- geo.go
+++ geo.go
@@ -4,11 +4,13 @@ import (
 	redigo "github.com/garyburd/redigo/redis"
 )
 
+// Position 经纬度位置
 type Position struct {
 	Lng float64
 	Lat float64
 }
 
+// Location 位置
 type Location struct {
 	Member   string
 	Distance string
@@ -19,6 +21,7 @@ type Location struct {
 
 //region 1.6 Geo
 
+// GeoAdd redis命令
 func (p *pool) GeoAdd(key string, longitude, latitude float64, member interface{}, args ...interface{}) (createNum int, err error) {
 	var (
 		params = make([]interface{}, 0, len(args)+4)
@@ -28,6 +31,7 @@ func (p *pool) GeoAdd(key string, longitude, latitude float64, member interface{
 	return redigo.Int(p.Do("GEOADD", params...))
 }
 
+// GeoHash redis命令
 func (p *pool) GeoHash(key string, members ...interface{}) (hashList []string, err error) {
 	var (
 		args = make([]interface{}, 0, len(members)+1)
@@ -39,14 +43,17 @@ func (p *pool) GeoHash(key string, members ...interface{}) (hashList []string, e
 	return redigo.Strings(p.Do("GEOHASH", args...))
 }
 
+// GeoDel redis命令
 func (p *pool) GeoDel(key string, members ...interface{}) (removeNum int, err error) {
 	return p.ZRem(key, members)
 }
 
+// GeoDist redis命令
 func (p *pool) GeoDist(key string, member1, member2 interface{}, unit string) (distance string, err error) {
 	return String(p.Do("GEODIST", key, member1, member2, unit))
 }
 
+// GeoPos redis命令
 func (p *pool) GeoPos(key string, members ...interface{}) (positionList []Position, err error) {
 	var (
 		args = make([]interface{}, 0, len(members)+1)
@@ -58,6 +65,7 @@ func (p *pool) GeoPos(key string, members ...interface{}) (positionList []Positi
 	return Positions(p.Do("GEOPOS", args...))
 }
 
+// GeoRadius redis命令
 func (p *pool) GeoRadius(key string, longitude, latitude float64, radius interface{}, unit string, count int, sort string) (locationList []Location, err error) {
 	if sort == "" {
 		sort = "ASC"
@@ -70,6 +78,7 @@ func (p *pool) GeoRadius(key string, longitude, latitude float64, radius interfa
 	return Locations(p.Do("GEORADIUS", key, longitude, latitude, radius, unit, "WITHCOORD", "WITHHASH", "WITHDIST", "COUNT", count, sort))
 }
 
+// GeoRadiusByMember redis命令
 func (p *pool) GeoRadiusByMember(key string, member interface{}, radius interface{}, unit string, count int, sort string) (locationList []Location, err error) {
 	if sort == "" {
 		sort = "ASC"
@@ -85,6 +94,7 @@ func (p *pool) GeoRadiusByMember(key string, member interface{}, radius interfac
 
 //endregion
 
+// GeoAdd redis命令
 func (m *multi) GeoAdd(key string, longitude, latitude float64, member interface{}, args ...interface{}) Multi {
 	var (
 		params = make([]interface{}, 0, len(args)+4)
@@ -95,6 +105,7 @@ func (m *multi) GeoAdd(key string, longitude, latitude float64, member interface
 	return m
 }
 
+// GeoHash redis命令
 func (m *multi) GeoHash(key string, members ...interface{}) Multi {
 	var (
 		args = make([]interface{}, 0, len(members)+1)
@@ -107,15 +118,18 @@ func (m *multi) GeoHash(key string, members ...interface{}) Multi {
 	return m
 }
 
+// GeoDel redis命令
 func (m *multi) GeoDel(key string, members ...interface{}) Multi {
 	return m.ZRem(key, members...)
 }
 
+// GeoDist redis命令
 func (m *multi) GeoDist(key string, member1, member2 interface{}, unit string) Multi {
 	m.cmdList = append(m.cmdList, Cmd{cmd: "GEODIST", args: []interface{}{key, member1, member2, unit}})
 	return m
 }
 
+// GeoPos redis命令
 func (m *multi) GeoPos(key string, members ...interface{}) Multi {
 	var (
 		args = make([]interface{}, 0, len(members)+1)
@@ -128,6 +142,7 @@ func (m *multi) GeoPos(key string, members ...interface{}) Multi {
 	return m
 }
 
+// GeoRadius redis命令
 func (m *multi) GeoRadius(key string, longitude, latitude float64, radius interface{}, unit string, count int, sort string) Multi {
 	if sort == "" {
 		sort = "ASC"
@@ -144,6 +159,7 @@ func (m *multi) GeoRadius(key string, longitude, latitude float64, radius interf
 	return m
 }
 
+// GeoRadiusByMember redis命令
 func (m *multi) GeoRadiusByMember(key string, member interface{}, radius interface{}, unit string, count int, sort string) Multi {
 	if sort == "" {
 		sort = "ASC"
